486de4117785a051be0de8e3a738e43a0f322f5f
Sebastian Baltser <sebastian.baltser@gmail.com>
2022-02-12 11:38:28 +0100

    Create tests for SinglePeriodEconomy

--- code/single-period-model/tests/spm/test_spm.py
+++ code/single-period-model/tests/spm/test_spm.py
@@ -2,6 +2,7 @@ import math
 import pytest
 from spm.spm import (
     State,
+    SinglePeriodEconomy,
 )
 
 
@@ -28,3 +29,29 @@ class TestState:
     ])
     def test_present_value(self, state, future_value, expected):
         assert math.isclose(state.present_value(future_value), expected)
+
+
+class TestSinglePeriodEconomy:
+    @pytest.mark.parametrize("economy_probabilities, error_match", [
+        ([0.5, 0.4], "must sum to 1.0"),
+        ([0.2, 0.9, -0.1], "must be between 0 and 1"),
+    ])
+    def test_invalid_probability(self, economy_probabilities, error_match):
+        economy = {State(i, 0.1): p for i, p in enumerate(economy_probabilities)}
+        with pytest.raises(ValueError, match=error_match):
+            SinglePeriodEconomy(economy)
+
+    @pytest.mark.parametrize("economy, expected", [
+        ({State(1, 0.50): 0.5, State(2, 0.48): 0.5}, 0.98),
+        ({State(1, 0.35): 0.3, State(2, 0.35): 0.3, State(3, 0.20): 0.4}, 0.90),
+    ])
+    def test_discount_factor(self, economy, expected):
+        economy = SinglePeriodEconomy(economy)
+        assert math.isclose(economy.discount_factor, expected)
+
+    @pytest.mark.parametrize("economy, expected", [
+        ({State(1, 0.50): 0.5, State(2, 0.48): 0.5}, 1.0/0.98),
+    ])
+    def test_risk_free_rate(self, economy, expected):
+        economy = SinglePeriodEconomy(economy)
+        assert math.isclose(economy.risk_free_rate, expected)
