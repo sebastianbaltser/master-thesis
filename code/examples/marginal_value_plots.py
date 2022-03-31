94b54eae3188c785bf26d3c0f1247a3b6a9655d4
Sebastian Baltser <sebastian.baltser@gmail.com>
2022-03-31 17:19:13 +0200

    Fix sharing of globals

--- code/examples/marginal_value_plots.py
+++ code/examples/marginal_value_plots.py
@@ -44,19 +44,18 @@ def get_legacy_equity():
     Equity(firm_assets, debt_face_value=debt.face_value)
 
 
-def get_new_firm_from_debt_financed_option(option, option_price):
-    firm_assets = get_firm_assets()
+def get_new_firm_from_debt_financed_option(firm: Firm, option, option_price):
+    firm_assets = firm.assets
     new_firm_assets = firm_assets + option
-    debt = get_legacy_debt()
-    new_debt = DebtPariPassu(new_firm_assets, other_face_value=debt.face_value, present_value=option_price)
+    debt_face_value = firm.debt_face_value
+    new_debt = DebtPariPassu(new_firm_assets, other_face_value=debt_face_value, present_value=option_price)
 
-    total_face_value = debt.face_value + new_debt.face_value
+    total_face_value = debt_face_value + new_debt.face_value
     return Firm(new_firm_assets, debt_face_value=total_face_value)
 
 
-def marginal_shareholder_value_of_debt_financing(option, option_price):
-    economy = get_economy()
-    firm = get_new_firm_from_debt_financed_option(option, option_price)
+def marginal_shareholder_value_of_debt_financing(economy: SinglePeriodEconomy, firm: Firm, option, option_price):
+    firm = get_new_firm_from_debt_financed_option(firm, option, option_price)
     default_probability = economy.risk_neutral_expectation(economy.map(firm.is_default_state))
     expected_payoff = economy.risk_neutral_expectation(option)
 
@@ -72,11 +71,32 @@ def marginal_shareholder_value_of_debt_financing(option, option_price):
     return expected_profit - payoff_default_covariance - marginal_excess_return
 
 
-def plot_marginal_shareholder_value_debt_financing():
-    option = Asset(States(get_states(), [10, 10, 10, 10, 10]))
-    option_price_range = np.arange(1, 30, 0.01)
+def get_new_firm_from_equity_financed_option(firm: Firm, option):
+    firm_assets = firm.assets
+    new_firm_assets = firm_assets + option
+    debt_face_value = firm.debt_face_value
 
-    marginal_shareholder_values = [marginal_shareholder_value_of_debt_financing(option, option_price)
+    return Firm(new_firm_assets, debt_face_value=debt_face_value)
+
+
+def marginal_shareholder_value_of_equity_financing(economy: SinglePeriodEconomy, firm: Firm, option, option_price):
+    firm = get_new_firm_from_equity_financed_option(firm, option)
+    default_probability = economy.risk_neutral_expectation(economy.map(firm.is_default_state))
+    expected_payoff = economy.risk_neutral_expectation(option)
+
+    expected_profit = (1 - default_probability) * (expected_payoff * economy.discount_factor - option_price)
+
+    payoff_default_covariance = economy.risk_neutral_covariance(option, economy.map(firm.is_default_state))
+    payoff_default_covariance *= economy.discount_factor
+
+    shareholder_direct_loss = default_probability * option_price
+
+    return expected_profit - payoff_default_covariance - shareholder_direct_loss
+
+
+
+def plot_marginal_shareholder_value_debt_financing(economy, firm, option, option_price_range):
+    marginal_shareholder_values = [marginal_shareholder_value_of_debt_financing(economy, firm, option, option_price)
                                    for option_price in option_price_range]
 
     fig, ax = plt.subplots()
@@ -89,12 +109,16 @@ def plot_marginal_shareholder_value_debt_financing():
 
 
 if __name__ == '__main__':
+    economy = SinglePeriodEconomy(States(get_states(), [0.1, 0.3, 0.3, 0.25, 0.05]))
     option = Asset(States(get_states(), [10, 10, 10, 10, 10]))
+    firm = Firm(Asset(States(get_states(), [120, 110, 100, 95, 60])), debt_face_value=80)
     option_price = 9.90
-    marginal_shareholder_value = marginal_shareholder_value_of_debt_financing(option, option_price)
+    marginal_shareholder_value = marginal_shareholder_value_of_debt_financing(economy, firm, option, option_price)
     print(f"Marginal Shareholder Value Debt Financing: {marginal_shareholder_value:.4f}")
-    marginal_shareholder_value = marginal_shareholder_value_of_equity_financing(option, option_price)
+    marginal_shareholder_value = marginal_shareholder_value_of_equity_financing(economy, firm, option, option_price)
     print(f"Marginal Shareholder Value Equity Financing: {marginal_shareholder_value:.4f}")
 
-    fig = plot_marginal_shareholder_value_debt_financing()
+    option = Asset(States(get_states(), [10, 10, 10, 10, 10]))
+    option_price_range = np.arange(1, 30, 0.01)
+    fig = plot_marginal_shareholder_value_debt_financing(economy, firm, option, option_price_range)
     plt.show()
