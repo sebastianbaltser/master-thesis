d08df9f347d0af0bd3d35f1308e386d2e1491983
Sebastian Baltser <sebastian.baltser@gmail.com>
2022-04-04 13:07:00 +0200

    Add marginal value of cash funding

--- code/single-period-model/spm/marginal_value_plots.py
+++ code/single-period-model/spm/marginal_value_plots.py
@@ -41,6 +41,26 @@ def marginal_shareholder_value_of_debt_financing(economy: SinglePeriodEconomy, f
     return expected_profit - payoff_default_covariance - marginal_excess_return
 
 
+def get_new_firm_from_cash_financed_option(economy: SinglePeriodEconomy, firm: Firm, option, option_price):
+    firm_assets = firm.assets
+    forward_discounted_option_price = option_price / economy.discount_factor
+    new_firm_assets = firm_assets + option + (- forward_discounted_option_price)
+
+    return Firm(new_firm_assets, debt_face_value=firm.debt_face_value)
+
+
+def marginal_shareholder_value_of_cash_financing(economy: SinglePeriodEconomy, firm: Firm, option, option_price):
+    default_probability = economy.risk_neutral_expectation(economy.map(firm.is_default_state))
+    expected_payoff = economy.risk_neutral_expectation(option)
+
+    expected_profit = (1 - default_probability) * (expected_payoff * economy.discount_factor - option_price)
+
+    payoff_default_covariance = economy.risk_neutral_covariance(option, economy.map(firm.is_default_state))
+    payoff_default_covariance *= economy.discount_factor
+
+    return expected_profit - payoff_default_covariance
+
+
 def get_new_firm_from_equity_financed_option(firm: Firm, option):
     firm_assets = firm.assets
     new_firm_assets = firm_assets + option
@@ -66,7 +86,7 @@ def marginal_shareholder_value_of_equity_financing(economy: SinglePeriodEconomy,
 def plot_marginal_shareholder_value(economy, base_firm, option, option_price_range) -> plt.Figure:
     firms = [get_new_firm_from_debt_financed_option(base_firm, option, option_price)
              for option_price in option_price_range]
-    firm_option_price_pairs = list(zip(firms, option_price_range))
+    firm_option_price_pairs = zip(firms, option_price_range)
     g_debt = [marginal_shareholder_value_of_debt_financing(economy, firm, option, option_price)
               for firm, option_price in tqdm.tqdm(firm_option_price_pairs, desc="Debt Funding", total=len(firms))]
 
@@ -76,9 +96,16 @@ def plot_marginal_shareholder_value(economy, base_firm, option, option_price_ran
     g_equity = [marginal_shareholder_value_of_equity_financing(economy, firm, option, option_price)
                 for option_price in tqdm.tqdm(option_price_range, desc="Equity Funding")]
 
-    fig, ax = plt.subplots()
+    firms = [get_new_firm_from_cash_financed_option(economy, base_firm, option, option_price)
+             for option_price in option_price_range]
+    firm_option_price_pairs = zip(firms, option_price_range)
+    g_cash = [marginal_shareholder_value_of_cash_financing(economy, firm, option, option_price)
+              for firm, option_price in tqdm.tqdm(firm_option_price_pairs, desc="Cash Funding", total=len(firms))]
+
+    fig, ax = plt.subplots(figsize=(12, 6))
     ax.plot(option_price_range, g_debt, label="Debt Funding")
     ax.plot(option_price_range, g_equity, label="Equity Funding")
+    ax.plot(option_price_range, g_cash, label="Cash Funding")
     ax.plot(option_price_range, credit_spreads, label="Debt Credit Spread")
     ax.set_xlabel("Option Price")
     ax.set_ylabel("Marginal Shareholder Value")
